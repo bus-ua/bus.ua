9b1677e89899535c0f6e26ec90bee739db292a6b
bus-ua <96583481+bus-ua@users.noreply.github.com>
2021-12-23 16:22:09 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,31 @@
+const opens = document.getElementById('open');
+const containerInfo = document.querySelector('.container_info');
+const downTop = document.querySelector('.down_top');
+
+
+let Opens = true;
+
+
+const addOpen = (add) => {
+    // console.log(add)
+    for (let i = 0; i < add.children.length; i++) {
+        newArr = add.children[1];
+        newArr2 = add.children[0].children[1].children[0];
+
+        // alert('asd')
+
+    }
+    console.log(newArr)
+
+    if (Opens === true) {
+        newArr.classList.remove('active_menu');
+        newArr2.classList.add('down');
+        Opens = false;
+    } else {
+        newArr.classList.add('active_menu');
+        newArr2.classList.remove('down');
+        Opens = true;
+
+    }
+
+}
