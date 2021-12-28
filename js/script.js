a7e12a95f45e8ae6ccffc38696eeecc347d5f58e
bus-ua <96583481+bus-ua@users.noreply.github.com>
2021-12-28 15:25:28 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -24,131 +24,187 @@ const addOpen = (add) => {
     }
 
 }
-var style = document.createElement('style');
-style.innerHTML = `
-  .sf-snow-flake {
-position: fixed;
-top: -20px;
-z-index: 99999;
-}
-.sf-snow-anim {
-top: 110%;
-}
-  `;
-document.head.appendChild(style);
 
-/*
-Copyright (c) 2014 Matthew Hudson - MIT License
-device.js 0.1.61
-*/
-(function () { var a, b, c, d, e, f, g, h, i, j; a = window.device, window.device = {}, c = window.document.documentElement, j = window.navigator.userAgent.toLowerCase(), device.ios = function () { return device.iphone() || device.ipod() || device.ipad() }, device.iphone = function () { return d("iphone") }, device.ipod = function () { return d("ipod") }, device.ipad = function () { return d("ipad") }, device.android = function () { return d("android") }, device.androidPhone = function () { return device.android() && d("mobile") }, device.androidTablet = function () { return device.android() && !d("mobile") }, device.blackberry = function () { return d("blackberry") || d("bb10") || d("rim") }, device.blackberryPhone = function () { return device.blackberry() && !d("tablet") }, device.blackberryTablet = function () { return device.blackberry() && d("tablet") }, device.windows = function () { return d("windows") }, device.windowsPhone = function () { return device.windows() && d("phone") }, device.windowsTablet = function () { return device.windows() && d("touch") && !device.windowsPhone() }, device.fxos = function () { return (d("(mobile;") || d("(tablet;")) && d("; rv:") }, device.fxosPhone = function () { return device.fxos() && d("mobile") }, device.fxosTablet = function () { return device.fxos() && d("tablet") }, device.meego = function () { return d("meego") }, device.cordova = function () { return window.cordova && "file:" === location.protocol }, device.nodeWebkit = function () { return "object" == typeof window.process }, device.mobile = function () { return device.androidPhone() || device.iphone() || device.ipod() || device.windowsPhone() || device.blackberryPhone() || device.fxosPhone() || device.meego() }, device.tablet = function () { return device.ipad() || device.androidTablet() || device.blackberryTablet() || device.windowsTablet() || device.fxosTablet() }, device.desktop = function () { return !device.tablet() && !device.mobile() }, device.portrait = function () { return window.innerHeight / window.innerWidth > 1 }, device.landscape = function () { return window.innerHeight / window.innerWidth < 1 }, device.noConflict = function () { return window.device = a, this }, d = function (a) { return -1 !== j.indexOf(a) }, f = function (a) { var b; return b = new RegExp(a, "i"), c.className.match(b) }, b = function (a) { return f(a) ? void 0 : c.className += " " + a }, h = function (a) { return f(a) ? c.className = c.className.replace(a, "") : void 0 }, device.ios() ? device.ipad() ? b("ios ipad tablet") : device.iphone() ? b("ios iphone mobile") : device.ipod() && b("ios ipod mobile") : b(device.android() ? device.androidTablet() ? "android tablet" : "android mobile" : device.blackberry() ? device.blackberryTablet() ? "blackberry tablet" : "blackberry mobile" : device.windows() ? device.windowsTablet() ? "windows tablet" : device.windowsPhone() ? "windows mobile" : "desktop" : device.fxos() ? device.fxosTablet() ? "fxos tablet" : "fxos mobile" : device.meego() ? "meego mobile" : device.nodeWebkit() ? "node-webkit" : "desktop"), device.cordova() && b("cordova"), e = function () { return device.landscape() ? (h("portrait"), b("landscape")) : (h("landscape"), b("portrait")) }, i = "onorientationchange" in window, g = i ? "orientationchange" : "resize", window.addEventListener ? window.addEventListener(g, e, !1) : window.attachEvent ? window.attachEvent(g, e) : window[g] = e, e() }).call(this);
-/*
-snowFlurry JS - version 2.0
-Copyright Ð’Â© 2015 S.W. Clough (https://www.html5andbeyond.com)
-Licensed Under MIT
-*/
-(function ($) {
-    $.fn.snowFlurry = function (options) {
-        var s = $.extend({
-            maxSize: 5,
-            numberOfFlakes: 25,
-            minSpeed: 10,
-            maxSpeed: 15,
-            color: '#fff',
-            timeout: 0
-        }, options);
-        var windowWidth = $(window).innerWidth(),
-            WidthArray = [],
-            DelayArray = [],
-            animateArray = [],
-            flakeSize = [],
-            snowInterval;
-        if (s.maxSize <= 10) {
-            for (var i = 1; i < s.maxSize; i++) {
-                flakeSize.push(i);
-            }
-        } else {
-            for (var i = 1; i < 10; i++) {
-                flakeSize.push(i);
-            }
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+30
+31
+32
+33
+34
+35
+36
+37
+38
+39
+40
+41
+42
+43
+44
+45
+46
+47
+48
+49
+50
+51
+52
+53
+54
+55
+56
+57
+58
+59
+60
+61
+62
+63
+64
+65
+66
+67
+68
+69
+70
+71
+72
+73
+74
+75
+76
+77
+78
+79
+80
+81
+82
+83
+84
+85
+86
+87
+88
+89
+90
+91
+92
+93
+////////////////////////
+///////// Настройки
+////////////////////////
+
+// количество снежинок, которое будет на экране одновременно.
+let snowmax = 40
+
+// Цвета для снежинок. Для каждой конкретной снежинки цвет выбирается случайно из этого массива.
+let snowcolor = new Array("#b9dff5", "#7fc7ff", "#7fb1ff", "#7fc7ff", "#b9dff5")
+
+// Шрифт для снежинок
+let snowtype = new Array("Times")
+
+// Символ (*) и есть снежинка, в место нее можно вставить любой другой символ.
+let snowletter = "&#10052;"
+
+// Скорость движения снежинок (от 0.3 до 2)
+let sinkspeed = 0.4
+
+// Максимальный размер для снежинок
+let snowmaxsize = 40
+
+// Минимальный размер для снежинок
+let snowminsize = 10
+
+// Зона для снежинок
+// 1 для всей страницы, 2 в левой части страницы
+// 3 в центральной части, 4 в правой части страницы
+let snowingzone = 1
+
+////////////////////////
+///////// Конец настроек
+////////////////////////
+
+let snow = new Array();
+let marginbottom;
+let marginright;
+let timer;
+let i_snow = 0;
+let x_mv = new Array();
+let crds = new Array();
+let lftrght = new Array();
+function randommaker(range) {
+    rand = Math.floor(range * Math.random());
+    return rand;
+}
+function initsnow() {
+    marginbottom = document.documentElement.clientHeight + 50
+    marginright = document.body.clientWidth - 15
+    let snowsizerange = snowmaxsize - snowminsize
+    for (i = 0; i <= snowmax; i++) {
+        crds[i] = 0;
+        lftrght[i] = Math.random() * 15;
+        x_mv[i] = 0.03 + Math.random() / 10;
+        snow[i] = document.getElementById("s" + i)
+        snow[i].style.fontFamily = snowtype[randommaker(snowtype.length)]
+        snow[i].size = randommaker(snowsizerange) + snowminsize
+        snow[i].style.fontSize = snow[i].size + 'px';
+        snow[i].style.color = snowcolor[randommaker(snowcolor.length)]
+        snow[i].style.zIndex = 1000
+        snow[i].sink = sinkspeed * snow[i].size / 5
+        if (snowingzone == 1) { snow[i].posx = randommaker(marginright - snow[i].size) }
+        if (snowingzone == 2) { snow[i].posx = randommaker(marginright / 2 - snow[i].size) }
+        if (snowingzone == 3) { snow[i].posx = randommaker(marginright / 2 - snow[i].size) + marginright / 4 }
+        if (snowingzone == 4) { snow[i].posx = randommaker(marginright / 2 - snow[i].size) + marginright / 2 }
+        snow[i].posy = randommaker(2 * marginbottom - marginbottom - 2 * snow[i].size)
+        snow[i].style.left = snow[i].posx + 'px';
+        snow[i].style.top = snow[i].posy + 'px';
+    }
+    movesnow()
+}
+function movesnow() {
+    for (i = 0; i <= snowmax; i++) {
+        crds[i] += x_mv[i];
+        snow[i].posy += snow[i].sink
+        snow[i].style.left = snow[i].posx + lftrght[i] * Math.sin(crds[i]) + 'px';
+        snow[i].style.top = snow[i].posy + 'px';
+
+        if (snow[i].posy >= marginbottom - 2 * snow[i].size || parseInt(snow[i].style.left) > (marginright - 3 * lftrght[i])) {
+            if (snowingzone == 1) { snow[i].posx = randommaker(marginright - snow[i].size) }
+            if (snowingzone == 2) { snow[i].posx = randommaker(marginright / 2 - snow[i].size) }
+            if (snowingzone == 3) { snow[i].posx = randommaker(marginright / 2 - snow[i].size) + marginright / 4 }
+            if (snowingzone == 4) { snow[i].posx = randommaker(marginright / 2 - snow[i].size) + marginright / 2 }
+            snow[i].posy = 0
         }
-        for (var i = 0; i < windowWidth - 20; i++) {
-            WidthArray.push(i);
-        }
-        for (var i = 0; i < s.numberOfFlakes; i++) {
-            $('<div class="sf-snow-flake"></div>').appendTo('body');
-        }
-        for (var i = 0; i < 10; i++) {
-            DelayArray.push(i);
-        }
-        for (var i = s.minSpeed; i < s.maxSpeed; i++) {
-            animateArray.push(i);
-        }
-        function getRandomFlakeSize() {
-            var item = flakeSize[Math.floor(Math.random() * flakeSize.length)];
-            return item;
-        }
-        function getRandomPosition() {
-            var item = WidthArray[Math.floor(Math.random() * WidthArray.length)];
-            return item;
-        }
-        function getRandomDelay() {
-            var item = DelayArray[Math.floor(Math.random() * DelayArray.length)];
-            return item * 1000;
-        }
-        function getRandomAnimation() {
-            var item = animateArray[Math.floor(Math.random() * animateArray.length)];
-            return item * 1000;
-        }
-        $('.sf-snow-flake').each(function () {
-            var elem = $(this);
-            elem.attr('data-speed', getRandomAnimation());
-            elem.attr('data-delay', getRandomDelay());
-            var elemSpeed = elem.attr('data-speed'),
-                elemDelay = elem.attr('data-delay');
-            var flakeSize = getRandomFlakeSize();
-            elem.css({
-                'width': flakeSize,
-                'height': flakeSize,
-                'border-radius': flakeSize / 2,
-                'background-color': s.color,
-                'box-shadow': '0 0 2px 1px' + s.color
-            })
-            function activateAnim() {
-                setTimeout(function () {
-                    elem.css('left', getRandomPosition());
-                    elem.addClass('sf-snow-anim');
-                    elem.css('transition', 'top ' + elemSpeed / 1000 + 's linear');
-                    setTimeout(function () {
-                        elem.css('transition', '');
-                        elem.removeClass('sf-snow-anim');
-                    }, elemSpeed);
-                }, elemDelay);
-            }
-            if (device.mobile() || device.tablet() || Modernizr.touch || $('html').hasClass('no-csstransitions')) { } else if (device.desktop()) {
-                activateAnim();
-                snowInterval = setInterval(function () {
-                    activateAnim();
-                }, +elemDelay + +elemSpeed);
-            }
-            if (s.timeout != 0) {
-                setTimeout(function () {
-                    clearInterval(snowInterval);
-                    $('.sf-snow-flake').fadeOut(1500, function () {
-                        $(this).remove();
-                    })
-                }, s.timeout * 1000);
-            }
-        });
-    };
-}(jQuery));
-jQuery(document).ready(function ($) {
-    $(document).snowFlurry({
-        maxSize: 10,
-        numberOfFlakes: 100,
-        minSpeed: 10,
-        maxSpeed: 20,
-        color: '#fff',
-        timeout: 0
-    });
-});
+    }
+    let timer = setTimeout("movesnow()", 50)
+}
+
+for (i = 0; i <= snowmax; i++) {
+    document.body.insertAdjacentHTML('beforeend', "<span id='s" + i + "' style='user-select:none;position:fixed;top:-" + snowmaxsize + "'>" + snowletter + "</span>")
+}
+window.onload = initsnow 
